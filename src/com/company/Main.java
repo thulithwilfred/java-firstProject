de26ea5b42b1ba6eacd90cef515dc24ac5f9cb29
Thulith W Mallawa <thulith.mallawa@uqconnect.edu.au>
2021-01-12 22:30:07 +1000

    Editting source file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("testingCommit - X64");
+        System.out.println("i think im getting the hang of this");
     }
 }
