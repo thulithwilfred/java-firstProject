369f66825903764254666602c4ad434aac068709
Thulith W Mallawa <thulith.mallawa@uqconnect.edu.au>
2021-01-19 18:04:38 +1000

    /* Finito AmigosCode Java Course 1 */

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,77 @@
 package com.company;
+import jdk.jshell.Snippet;
+
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    /* Arbitrary Person Class */
+    static class Person {
+        /* Properties of a Class */
+        String name, height, age, sex, weight;
 
+        /* Creating an Object by passing properties */
+        Person(String name, String height, String age, String sex, String weight) {
+            this.age = age;
+            this.name = name;
+            this.height = height;
+            this.sex = sex;
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Tule", "183", "23", "M", "78.5");
+        Person person2 = new Person("Ash", "154", "69", "F", "55");
+
+        Person person3 = new Person("Tule", "183", "23", "M", "78.5");
+
+        System.out.println(person1.age.equals(person3.age));
+
+        /* printDate();
+        Scanner scanned = new Scanner(System.in);
+        int reVal = getUserInput(scanned);
+        System.out.println("GoodBye" + ", RetVal:" + reVal); */
+
+    }
+
+    /* Prints Current Date and Time */
+    public static void printDate() {
         Date date = new Date();
         System.out.println(date);
+    }
 
-        /* Arrays */
+    /* Gets User Input and Prints the year in which they were born */
+    public static int getUserInput(Scanner scanned) {
 
-        int [] intArr = new int[10];
-        String [] strArr = new String [2];
+        char [] letters = {'a', 'b', 'c', 'd', 'e', 'a', 'v', 'a'};
+        int count = 0;
 
-
-
-        for (int i = 0; i < 10; ++i) {
-
-            intArr[i] = i;
-
-            if (i == 0) {
-                strArr[i] = "Hello";
-            } else if (i == 1) {
-                strArr[i] = "Mellow";
-            }
-
-        }
-
-        /* Enhanced For Loop */
-        for (String count : strArr) {
-            //System.out.println(count);
-
-            if (count.equals("Hello") || count.equals("Mellow")) {
-               // System.out.println("craze");
+        for (char c : letters) {
+            if (c == 'a') {
+                ++count;
             }
         }
 
-        for (int i = 0; i < intArr.length; i++) {
-            intArr[i] += 10;
+        while(true)
+
+        {
+            System.out.println("Age?");
+            int numA = scanned.nextInt();
+
+            if (numA == 6969) {
+                System.out.println(count);
+                return 69;
+
+            } else {
+                System.out.println(LocalDate.now().minusYears(numA).getYear());
+            }
         }
 
-        System.out.println(Arrays.toString(intArr));
 
     }
 }
